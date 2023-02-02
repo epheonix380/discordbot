d452c8feaa5d3b166c44f80a3a14a37ccfe6903d
nyannyunt <nyannyunt1234@gmail.com>
2023-02-01 22:15:36 -0800

    Staticfiles

--- backend/settings.py
+++ backend/settings.py
@@ -36,8 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
