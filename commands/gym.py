2fc10283b89909a7aeac2301e171516e7d36a549
nyannyunt <nyannyunt1234@gmail.com>
2023-07-12 22:17:43 -0700

    Zero

--- commands/gym.py
+++ commands/gym.py
@@ -221,7 +221,7 @@ async def handleGymStatus(message:discord.Message):
                 weekProgress = weekProgress + 1
             else:
                 totalOwed = totalOwed + 1
-    if len(weeks) == 0 or total == 0:
+    if len(weeks)-1 == 0 or total == 0:
         await message.channel.send("You need to log your activity for at least 1 day for status to be available. Log it using ,gym checkin")
     else:
         await message.channel.send(f"You have done exercise for {gymCount} out of {total} days. Thats {(gymCount*100)/total}%!\nYou have done 4 or more days of training in {weekProgress} out of {len(weeks)-1} weeks, thats {(weekProgress*100)/(len(weeks)-1)}%!\nThat means you only owe ${totalOwed*10} to the Japan trip fund.")
