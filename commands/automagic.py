eb37b15e9de3b9a1a6c83970c115def7d91f8c9e
nyannyunt <nyannyunt1234@gmail.com>
2025-07-11 21:50:59 -0700

    Changed to arrows

--- commands/automagic.py
+++ commands/automagic.py
@@ -20,7 +20,7 @@ async def automagic(message: discord.Message):
                 timezone = pytz.timezone(defaultTimeZone)
                 today = datetime.datetime.now(tz=timezone)
                 time = timezone.localize(datetime.datetime(year=today.year, month=today.month, day=today.day, hour=hour, minute=minute))
-                reply += f"{timeString}: <t:{math.floor(time.timestamp())}:t>\n"
+                reply += f"{timeString} **-->** <t:{math.floor(time.timestamp())}:t>\n"
     timeStringRaw = re.findall(r"\b(?:1[0-2]|[1-9])\s?[ap]m\b",message.content)
     for timeStringTuple in timeStringRaw:
         timeString = timeStringTuple if type(timeStringTuple) is str else timeStringTuple[0]
@@ -35,6 +35,6 @@ async def automagic(message: discord.Message):
                 timezone = pytz.timezone(defaultTimeZone)
                 today = datetime.datetime.now(tz=timezone)
                 time = timezone.localize(datetime.datetime(year=today.year, month=today.month, day=today.day, hour=hour, minute=0))
-                reply += f"{timeString} {ending}: <t:{math.floor(time.timestamp())}:t>\n"
+                reply += f"{timeString} {ending} **-->** <t:{math.floor(time.timestamp())}:t>\n"
     if len(reply) > 0:
         await message.reply(reply)
